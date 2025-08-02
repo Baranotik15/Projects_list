2827cad16ed653b12207510c9e7bcae0097605f5
RuslanTopal <baranotik15@gmail.com>
2025-08-02 09:14:29 +0000

    some text changes

--- wheel_of_fortune/urls.py
+++ wheel_of_fortune/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.wheel_of_fortune, name='wheel_of_fortune'),
 ]
--- wheel_of_fortune/views.py
+++ wheel_of_fortune/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def wheel_of_fortune(request):
     return render(request, 'wheel_of_fortune/wheel_of_fortune_main_page.html')
